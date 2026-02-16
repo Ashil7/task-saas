85413df63e584057da2197db47b2e6789019c0ec
Ashil7 <ashilbenny555@gmail.com>
2026-02-16 22:51:15 +0530

    User,Organization and membership models implemented

--- SaasApp/admin.py
+++ SaasApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Organization, Membership
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Organization)
+admin.site.register(Membership)
+
+
+
--- SaasApp/models.py
+++ SaasApp/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email=models.EmailField(unique=True)
+    
+    
+    
+    USERNAME_FIELD='email'
+    REQUIRED_FIELDS=['username']
+    
+    def __str__(self):
+        return self.email   
+    
+class Organization(models.Model):
+    name=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Membership(models.Model):
+    ROLE_CHOICES=(
+        ("ADMIN",'admin'),
+        ("MANAGER",'member'),
+        ("MEMBER","member"),
+    )
+    
+    user =models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="memberships"
+    )
+    
+    organization=models.ForeignKey(
+        Organization,
+        on_delete=models.CASCADE,
+        related_name="memberships"
+    )
+    
+    role= models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default="MEMBER"
+    )
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together=("user","organization")
+        
+    def __str__(self):
+        return f"{self.user.email}-{self.organization.name}({self.role})"
